86787f5c6730da856d80650eb65e3f5e4e51928d
Qamar-Zaman <qamarzaman123pt@gmail.com>
2024-09-08 10:26:34 -0700

    first project

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -3,21 +3,21 @@ import Image from 'next/image'
 const page = () => {
   return (
     <div><figure className="md:flex bg-blue-400 circle-xl p-8 md:p-0 dark:bg-yellow-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/h.jpg" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/R.jpg" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-        My home is about 7marla and consist of 3story.There are 8 rooms and 2 kichan in it.tThere is a Gov.t college in the front of my house.
+        House no.15,Ataa colony ,Rahim yar Khan,Zip code 64200,Punjab..
           
-           .
+    .
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          Home
+          Punjab
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-           RYK
+          city, RYK
         </div>
       </figcaption>
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,21 +3,21 @@ import Image from 'next/image'
 const page = () => {
   return (
     <div><figure className="md:flex bg-blue-400 circle-xl p-8 md:p-0 dark:bg-yellow-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/h.jpg" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/c.jpg" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-        My home is about 7marla and consist of 3story.There are 8 rooms and 2 kichan in it.tThere is a Gov.t college in the front of my house.
+        contact no.03218637736 EMAIL address;qamarzaman123pt@gmail.com
           
-           .
+    .
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          Home
+          Contact
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-           RYK
+          Whatsapp, 03218637736
         </div>
       </figcaption>
     </div>
